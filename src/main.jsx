85fd5838ebe13eaf454368d050c8ba254b522b6a
ybabenkoJapp <ybabenko@jappware.com>
2025-05-01 20:35:48 +0300

    feat: update routing structure to include SidebarLayout component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import SidebarLayout from "../src/layouts/sidebarLayout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route element={<SidebarLayout />}>
+          <Route index element={<App />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
