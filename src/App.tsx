49f3ceb12f52874262d4cbffc7178ec7cbc24f2b
ybabenkoJapp <ybabenko@jappware.com>
2025-05-01 20:37:08 +0300

    refactor: simplify App component by removing unused imports and code

--- src/App.tsx
+++ src/App.tsx
@@ -4,123 +4,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { red } from "@mui/material/colors";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-import {
-  BrowserRouter as Router,
-  // Switch,
-  Route,
-  Link,
-  // useRouteMatch,
-  useParams,
-} from "react-router-dom";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  Box,
-  Toolbar,
-  Divider,
-  CssBaseline,
-  AppBar,
-  Typography,
-  ListItemButton,
-} from "@mui/material";
-import CardsItems from "./components/CardsItems";
-import UseRefExample from "./components/UseRefExample";
-import Counter from "./components/Counter";
-import BasicUseIdExample from "./buildInHooks/BacisUseIdExample";
-import { ThemeContext } from "@emotion/react";
-import ThemedButton from "./components/ThemedButton";
-import { MyThemeProvider } from "./buildInHooks/ThemeContext";
-import BasicUseEffectExample from "./buildInHooks/BasicUseEffectExample";
-
-const drawerWidth = 240;
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500],
-    },
-  },
-});
-
 export default function App() {
-  const [value, setValue] = React.useState("My test value");
-
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    setValue(e.target.value);
-  }
-
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Clipped drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-      >
-        <Toolbar />
-        <Divider />
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            {["Radio Group", "Buttons", "Raiting"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-        </Box>
-      </Drawer>
-      <ThemeProvider theme={theme}>
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, p: 3, maxWidth: "100%", pt: 8 }}
-        >
-          {/* <CardsItems /> */}
-          <UseRefExample />
-          <Counter count={0} />
-        </Box>
-        <br />
-        <br />
-        <br />
-        Input value: {value}
-        <BasicUseIdExample
-          label="Test label"
-          value={value}
-          onChange={onChangeHandler}
-        />
-        <BasicUseIdExample
-          label="Test label"
-          value={value}
-          onChange={onChangeHandler}
-        />
-        <MyThemeProvider>
-          <ThemedButton />
-        </MyThemeProvider>
-        <BasicUseEffectExample userId={1} />
-      </ThemeProvider>
-    </Box>
+    <div>
+      <h1>
+        <strong>App route</strong>
+      </h1>
+    </div>
   );
 }
