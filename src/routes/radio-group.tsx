fc9666971c31cebb0a65acf6f7ba344298fd4b87
ybabenkoJapp <ybabenko@jappware.com>
2025-05-02 11:05:06 +0300

    feat: enhance RadioGroupComponent with error handling and quiz functionality

--- src/routes/radio-group.tsx
+++ src/routes/radio-group.tsx
@@ -1,28 +1,109 @@
-import { Box, Radio } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Radio,
+  Stack,
+  RadioGroup,
+  FormControlLabel,
+} from "@mui/material";
 import * as React from "react";
 
-export default function RadioGroup() {
+export default function RadioGroupComponent() {
   const [selectedValue, setSelectedValue] = React.useState("a");
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState("Choose wisely");
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+    setHelperText(" ");
+    setError(false);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(event.target.value);
   };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (value === "best") {
+      setHelperText("You got it!");
+      setError(false);
+    } else if (value === "worst") {
+      setHelperText("Sorry, wrong answer!");
+      setError(true);
+    } else {
+      setHelperText("Please select an option.");
+      setError(true);
+    }
+  };
   return (
-    <Box>
-      <Radio
-        checked={selectedValue === "a"}
-        onChange={handleChange}
-        value="a"
-        name="radio-buttons"
-        inputProps={{ "aria-label": "A" }}
-      />
-      <Radio
-        checked={selectedValue === "b"}
-        onChange={handleChange}
-        value="b"
-        name="radio-buttons"
-        inputProps={{ "aria-label": "B" }}
-      />
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <Stack spacing={2} direction="row">
+        <Radio
+          checked={selectedValue === "a"}
+          onChange={handleChange}
+          value="a"
+          name="radio-buttons"
+          inputProps={{ "aria-label": "A" }}
+        />
+        <Radio
+          checked={selectedValue === "b"}
+          onChange={handleChange}
+          value="b"
+          name="radio-buttons"
+          inputProps={{ "aria-label": "B" }}
+        />
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </FormControl>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <form onSubmit={handleSubmit}>
+          <FormControl sx={{ m: 3 }} error={error} variant="standard">
+            <FormLabel id="demo-error-radios">Pop quiz: MUI is...</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-error-radios"
+              name="quiz"
+              value={value}
+              onChange={handleRadioChange}
+            >
+              <FormControlLabel
+                value="best"
+                control={<Radio />}
+                label="The best!"
+              />
+              <FormControlLabel
+                value="worst"
+                control={<Radio />}
+                label="The worst."
+              />
+            </RadioGroup>
+            <FormHelperText>{helperText}</FormHelperText>
+            <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
+              Check Answer
+            </Button>
+          </FormControl>
+        </form>
+      </Stack>
     </Box>
   );
 }
