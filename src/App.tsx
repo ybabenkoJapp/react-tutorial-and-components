47d279ee813aa8c2601169be80daba196e20317c
ybabenkoJapp <ybabenko@jappware.com>
2025-05-02 10:56:25 +0300

    feat: restructure App component to integrate theme provider and remove placeholder content

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,29 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { MyThemeProvider } from "./buildInHooks/ThemeContext";
+import ThemedButton from "./components/ThemedButton";
+import BasicUseEffectExample from "./buildInHooks/BasicUseEffectExample";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
+
 export default function App() {
   return (
     <div>
-      <h1>
-        <strong>App route</strong>
-      </h1>
+      <ThemeProvider theme={theme}>
+        <MyThemeProvider>
+          <ThemedButton />
+        </MyThemeProvider>
+        <BasicUseEffectExample userId={1} />
+      </ThemeProvider>
     </div>
   );
 }
