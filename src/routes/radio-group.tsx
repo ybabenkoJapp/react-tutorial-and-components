066782e01ce632c8ba4075db64a54419a9c832a6
ybabenkoJapp <ybabenko@jappware.com>
2025-05-02 10:56:45 +0300

    feat: implement RadioGroup component with state management for radio button selection

--- src/routes/radio-group.tsx
+++ src/routes/radio-group.tsx
@@ -1,9 +1,28 @@
+import { Box, Radio } from "@mui/material";
 import * as React from "react";
 
 export default function RadioGroup() {
+  const [selectedValue, setSelectedValue] = React.useState("a");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
   return (
-    <div>
-      <h1>Radio group component</h1>
-    </div>
+    <Box>
+      <Radio
+        checked={selectedValue === "a"}
+        onChange={handleChange}
+        value="a"
+        name="radio-buttons"
+        inputProps={{ "aria-label": "A" }}
+      />
+      <Radio
+        checked={selectedValue === "b"}
+        onChange={handleChange}
+        value="b"
+        name="radio-buttons"
+        inputProps={{ "aria-label": "B" }}
+      />
+    </Box>
   );
 }
