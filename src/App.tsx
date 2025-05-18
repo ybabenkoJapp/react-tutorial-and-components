510a07c59964602fc8c5449cef5fe34ed541c7a8
ybabenkoJapp <ybabenko@jappware.com>
2025-05-18 20:28:01 +0300

    refactor(App): replace MyThemeProvider with ToggleButtomThemeContext for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { MyThemeProvider } from "./buildInHooks/ThemeContext";
+import { ToggleButtomThemeContext } from "./buildInHooks/ToggleButtomThemeContext";
 import ThemedButton from "./components/ThemedButton";
 import { Button } from "@mui/material";
 import BasicButton from "./components/BasicButton";
@@ -11,29 +11,10 @@ import MuiComponentModifier from "./components/MuiComponentModifier";
 // import { deepmerge } from "@mui/utils"; // commented for a while
 
 // logic in app.tsx - should be moved to a custom context
-interface MyCustomTheme {
-  palette: {
-    primary: {
-      main: string;
-    };
-  };
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          variants: {
-            props: { variant: string };
-            style: Record<string, unknown>;
-          }[];
-        };
-      };
-    };
-  };
-}
 export default function App() {
   return (
     <div>
-      <MyThemeProvider>
+      <ToggleButtomThemeContext>
         <ThemedButton />
         <Button variant="text">Text</Button>
         <Button variant="contained">Contained</Button>
@@ -43,7 +24,7 @@ export default function App() {
           <Button variant="outlined" children="My outlined" />
           <Button variant="text" children="My text" />
         </MuiComponentModifier>
-      </MyThemeProvider>
+      </ToggleButtomThemeContext>
     </div>
   );
 }
