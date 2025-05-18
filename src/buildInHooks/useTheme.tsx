b3d006e8e6161b2ee00641841fd2beb26feac8d7
ybabenkoJapp <ybabenko@jappware.com>
2025-05-18 20:29:17 +0300

    fix(useTheme): update import to use ToggleButtomThemeContext for theme management

--- src/buildInHooks/useTheme.tsx
+++ src/buildInHooks/useTheme.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useContext } from "react";
-import { ThemeContext } from "./ThemeContext";
+import { ThemeContext } from "./ToggleButtomThemeContext";
 
 const useTheme = () => useContext(ThemeContext);
 
