0fd63a5e2b73190bd105a194abd6dede28cee21b
ybabenkoJapp <ybabenko@jappware.com>
2025-05-20 10:36:21 +0300

    feat(utilityTypes.ts): add utility types for MuiElement size, color, and variant

---
+++ src/appTypes/utilityTypes.ts
@@ -0,0 +1,5 @@
+type MuiElementSize = 'small' | 'medium' | 'large';
+type MuiElementColor = 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning' | 'inherit';
+type MuiElementVariant = 'text' | 'outlined' | 'contained';
+
+export type { MuiElementSize, MuiElementColor, MuiElementVariant };
