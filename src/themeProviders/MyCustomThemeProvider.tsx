f06a3f1fe62e64e68b0cfbf3d02b4751cebefb97
ybabenkoJapp <ybabenko@jappware.com>
2025-06-17 23:56:25 +0300

    refactor(MyCustomThemeProvider.tsx): enhance ts typing

--- src/themeProviders/MyCustomThemeProvider.tsx
+++ src/themeProviders/MyCustomThemeProvider.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "@mui/material/styles";
 import MyCustomTheme from "../appTypes/MyCustomTheme";
 import { MyCustomThemeContext } from "../themeContexts/MyCustomThemeContext";
+import MyCustomThemeContextInterface from "../appTypes/MyCustomThemeContextInterface";
 
 export const MyCustomThemeProvider = function ({
   themeContext,
   theme,
   children,
 }: {
-  themeContext: MyCustomTheme;
+  themeContext: MyCustomThemeContextInterface;
   theme: MyCustomTheme;
   children: React.ReactNode;
 }) {
