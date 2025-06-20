82e476cf4895707ff2eab708ed009a7cd59ec27b
ybabenkoJapp <ybabenko@jappware.com>
2025-06-20 16:31:44 +0300

    feat(ButtonStyleConfig.ts): add type declarations

---
+++ src/appTypes/ButtonStyleConfig.ts
@@ -0,0 +1,10 @@
+export default interface ButtonStyleConfig {
+  backgroundColor: string;
+  textColor: string;
+  hoverColor: string;
+  borderRadius: string;
+  padding: string; // Example, '6px 16px'
+  fontSize: string; // Example, '0.875rem'
+  boxShadow: string;
+  // Can add hover styles, for example: hoverBoxShadow
+}
