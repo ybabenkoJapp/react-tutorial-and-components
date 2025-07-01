545bd927ef02a5a77b8a2f67070085e694a71349
ybabenkoJapp <ybabenko@jappware.com>
2025-07-01 23:09:38 +0300

    refactor(ButtonStyleConfig.ts): coment and update code

--- src/appTypes/ButtonStyleConfig.ts
+++ src/appTypes/ButtonStyleConfig.ts
@@ -1,10 +1,25 @@
-export default interface ButtonStyleConfig {
+// commented due merging code start
+// export default interface ButtonStyleConfig {
+//   backgroundColor: string;
+//   textColor: string;
+//   hoverColor: string;
+//   borderRadius: string;
+//   padding: string; // Example, '6px 16px'
+//   fontSize: string; // Example, '0.875rem'
+//   boxShadow: string;
+  // Can add hover styles, for example: hoverBoxShadow
+// }
+// commented due merging code end
+
+// new code starts
+interface ButtonStyleConfig {
   backgroundColor: string;
   textColor: string;
   hoverColor: string;
   borderRadius: string;
-  padding: string; // Example, '6px 16px'
-  fontSize: string; // Example, '0.875rem'
+  padding: string;
+  fontSize: string;
   boxShadow: string;
-  // Can add hover styles, for example: hoverBoxShadow
 }
+export default ButtonStyleConfig;
+// new code ends
