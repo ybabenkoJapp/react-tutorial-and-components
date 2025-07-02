30c4ef831c44aa6d46ddb23e533d17b839ff9e79
ybabenkoJapp <ybabenko@jappware.com>
2025-07-02 22:43:04 +0300

    refactor(App.tsx): resolve todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import MyCustomThemeWrapper from "./themeWrappers/MyCustomThemeWrapper";
 import MyCustomThemeMainComponent from "./components/MyCustomThemeMainComponent";
 
 export default function App() {
-  // TODO state management
-  // Extend logic for designed theme
-  // Think about extending setting logic for the rest components
-  // Drag and drop event handling
-  // Advanced topic Import to file theme settings
- 
   return (
     <MyCustomThemeWrapper>
       <MyCustomThemeMainComponent />
