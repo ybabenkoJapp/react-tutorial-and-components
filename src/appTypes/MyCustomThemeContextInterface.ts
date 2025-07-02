178cfa730083ab000203b4ef0faeda50397ef0ca
ybabenkoJapp <ybabenko@jappware.com>
2025-07-02 23:46:12 +0300

    refactor(MyCustomThemeContextInterface.tsx): remove commented code

--- src/appTypes/MyCustomThemeContextInterface.ts
+++ src/appTypes/MyCustomThemeContextInterface.ts
@@ -1,27 +1,3 @@
-// commented due code merging start 
-
-// import { Theme } from "@mui/material";
-// import { MuiElementSize, MuiElementColor } from "./utilityTypes";
-// import ButtonStyleConfig from "./ButtonStyleConfig";
-
-// export default interface MyCustomThemeContextInterface {
-//   myCustomTheme: Theme;
-//   setMyCustomTheme: React.Dispatch<React.SetStateAction<Theme>>;
-//   updateMyCustomThemeSettings: (
-//     newMyCustomThemeSettings: Partial<Theme>,
-//   ) => void;
-//   componentSizeValue: MuiElementSize;
-//   componentColorValue: MuiElementColor;
-//   handleComponentSizeChange: (size: unknown) => void;
-//   handleComponentColorChange: (color: unknown) => void;
-//   themeContext?: any;
-//   buttonConfig?: any;
-//   updateButtonConfig?: (newConfig: Partial<ButtonStyleConfig>) => void;
-// }
-
-// commented due code merging end 
-
-// new code start 
 import { Theme } from "@mui/material";
 import { MuiElementColor, MuiElementSize } from "./utilityTypes";
 import ButtonStyleConfig from "./ButtonStyleConfig"; // Переконайтеся, що цей імпорт правильний
@@ -40,5 +16,3 @@ interface MyCustomThemeContextInterface {
 }
 
 export default MyCustomThemeContextInterface;
-
-// new code end
